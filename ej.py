1b817e33fc259f68e39d00f7146f21903f2d6910
gaitannn <kokisgaitan@gmail.com>
2014-10-29 18:31:37 -0600

    ol

---
+++ ej.py
@@ -0,0 +1,71 @@
+_author_ = 'gaitan'
+import urllib2
+def sumados():
+    print 10*20
+
+def division(a,b):
+    result = a/b
+    print result
+
+    def cast():
+        i=10
+        f=10.5
+        int(10.5)
+        print "funcion"
+
+    def main():
+        division(300.54, 2100.0)
+    sumados()
+
+    class Estudiante(object):
+        def __init__(self,nombre, edad):
+            self.nombre =nombre
+            self.edad =edad
+
+            def hola(self):
+                return self.nombre
+
+            def may(self):
+                if self.edad>=18:
+                    return True
+                else:
+                    return False
+def EXCEPTION():
+    try:
+        3/0
+    except ZeroDivisionError as e:
+
+        print "e "
+
+        def getWeb():
+            try
+                web = urllib2.urlopen("ww")
+
+
+
+
+                def main():
+                    e = Estudiante( "adry", 19)
+                    print "hola %s" % e.hola()
+                    if e.may():
+                        print "es mayor de edad"
+                    else:
+                        print "no es  menor de edad"
+
+
+                        contador = 0
+                        while contador <= 10:
+                            print contador
+                            contador +=1
+                    EXCEPTION()
+
+
+
+
+
+    if __name__ == "__main__":
+        main()
+        
+        
+    
+    
